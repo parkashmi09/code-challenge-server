7dee5328693192bd2d884fa772bb95bdc5c9e0c6
parkash <parkashmi66@gmail.com>
2023-12-07 10:36:21 +0530

    fix: broken issue

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import express from "express";
 import useRouter from "../server/routes/user.js";
-import Selector from "./models/sector.js";
-import { data } from "./utils/data.js";
 import cors from "cors"
-import path from "path"
 
 dotenv.config();
 
@@ -18,29 +15,10 @@ app.use(cors());
 
 app.use("/api", useRouter);
 
-// async function insertData(selectors) {
-//     try {
-//         if (mongoose.connection.readyState !== 1) {
-//             throw new Error("MongoDB connection is not open.");
-//         }
-
-//         const result = await Selector.insertMany(selectors);
-//     } catch (error) {
-//         console.error("Error inserting data:", error);
-//     }
-// }
-// const modifiedData = data?.map((sector) => ({
-//     name: sector.name,
-//     subGroups: sector.subGroups.map((subGroup) => ({ name: subGroup })),
-// }));
-
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         console.log("Database is connected");
-        // insertData(modifiedData);
-
-        // Selector.collection.drop()
     })
     .catch((error) => console.error("Error connecting to the database:", error));
 
